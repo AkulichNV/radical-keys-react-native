649eec7130d197f1788723ac88e4e2d003b52ebb
Natalia <sh.nat.vlad@gmail.com>
2025-02-26 19:05:00 +0500

    refactor: change evolutionSvgs.ts

--- assets/images/evolution/evolutionSvgs.ts
+++ assets/images/evolution/evolutionSvgs.ts
@@ -1,113 +1,113 @@
-import * as E4E00 from './4E00/E4E00';
-import * as E4E3F from './4E3F/E4E3F';
-import * as E4E28 from './4E28/E4E28';
-import * as E4E36 from './4E36/E4E36';
-import * as E4E59 from './4E59/E4E59';
-import * as E4E85 from './4E85/E4E85';
-import * as E4E8C from './4E8C/E4E8C';
-import * as E4EA0 from './4EA0/E4EA0';
-import * as E4EBA from './4EBA/E4EBA';
-import * as E513F from './513F/E513F';
-import * as E5165 from './5165/E5165';
-import * as E516B from './516B/E516B';
-import * as E5182 from './5182/E5182';
-import * as E5196 from './5196/E5196';
-import * as E51AB from './51AB/E51AB';
-import * as E51E0 from './51E0/E51E0';
-import * as E51F5 from './51F5/E51F5';
-import * as E5200 from './5200/E5200';
-import * as E529B from './529B/E529B';
-import * as E52F9 from './52F9/E52F9';
-import * as E5315 from './5315/E5315';
-import * as E531A from './531A/E531A';
-import * as E5338 from './5338/E5338';
-import * as E5341 from './5341/E5341';
-import * as E535C from './535C/E535C';
-import * as E5369 from './5369/E5369';
-import * as E5382 from './5382/E5382';
-import * as E53B6 from './53B6/E53B6';
-import * as E53C8 from './53C8/E53C8';
-import * as E53E3 from './53E3/E53E3';
-import * as E56D7 from './56D7/E56D7';
-import * as E571F from './571F/E571F';
-import * as E58EB from './58EB/E58EB';
-import * as E5902 from './5902/E5902';
-import * as E590A from './590A/E590A';
-import * as E5915 from './5915/E5915';
-import * as E5927 from './5927/E5927';
-import * as E5973 from './5973/E5973';
-import * as E5B50 from './5B50/E5B50';
-import * as E5B80 from './5B80/E5B80';
-import * as E5BF8 from './5BF8/E5BF8';
-import * as E5C0F from './5C0F/E5C0F';
-import * as E5C22 from './5C22/E5C22';
-import * as E5C38 from './5C38/E5C38';
-import * as E5C6E from './5C6E/E5C6E';
-import * as E5C71 from './5C71/E5C71';
-import * as E5DDB from './5DDB/E5DDB';
-import * as E5DE5 from './5DE5/E5DE5';
-import * as E5DF1 from './5DF1/E5DF1';
-import * as E5DFE from './5DFE/E5DFE';
-import * as E5E7A from './5E7A/E5E7A';
-import * as E5E7F from './5E7F/E5E7F';
-import * as E5E72 from './5E72/E5E72';
-import * as E5EF4 from './5EF4/E5EF4';
-import * as E5EFE from './5EFE/E5EFE';
-import * as E5F0B from './5F0B/E5F0B';
-import * as E5F13 from './5F13/E5F13';
-import * as E5F50 from './5F50/E5F50';
-import * as E5F61 from './5F61/E5F61';
-import * as E5F73 from './5F73/E5F73';
-import * as E5FC3 from './5FC3/E5FC3';
-import * as E6208 from './6208/E6208';
-import * as E65A4 from './65A4/E65A4';
-import * as E65B9 from './65B9/E65B9';
-import * as E624B from './624B/E624B';
-import * as E652F from './652F/E652F';
-import * as E6237 from './6236/E6237';
-import * as E6534 from './6534/E6534';
-import * as E6587 from './6587/E6587';
-import * as E6597 from './6597/E6597';
-import * as E6B20 from './6B20/E6B20';
-import * as E6B62 from './6B62/E6B62';
-import * as E6B79 from './6B79/E6B79';
-import * as E6BB3 from './6BB3/E6BB3';
-import * as E6BCB from './6BCB/E6BCB';
-import * as E65E0 from './65E0/E65E0';
-import * as E65E5 from './65E5/E65E5';
-import * as E66F0 from './66F0/E66F0';
-import * as E6708 from './6708/E6708';
-import * as E6728 from './6728/E6728';
-import * as E6BD4 from './6BD4/E6BD4';
-import * as E6BDB from './6BDB/E6BDB';
-import * as E6C0F from './6C0F/E6C0F';
-import * as E6C14 from './6C14/E6C14';
-import * as E6C34 from './6C34/E6C34';
-import * as E706B from './706B/E706B';
-import * as E722A from './722A/E722A';
-import * as E723B from './723B/E723B';
-import * as E723F from './723F/E723F';
-import * as E7236 from './7236/E7236';
-import * as E72AC from './72AC/E72AC';
-import * as E74DC from './74DC/E74DC';
-import * as E74E6 from './74E6/E74E6';
-import * as E725B from './725B/E725B';
-import * as E751F from './751F/E751F';
-import * as E7247 from './7247/E7247';
-import * as E7259 from './7259/E7259';
-import * as E7384 from './7384/E7384';
-import * as E7389 from './7389/E7389';
-import * as E7518 from './7518/E7518';
-import * as E76AE from './76AE/E76AE';
-import * as E76BF from './76BF/E76BF';
-import * as E76EE from './76EE/E76EE';
-import * as E77DB from './77DB/E77DB';
-import * as E758B from './758B/E758B';
-import * as E767D from './767D/E767D';
-import * as E7528 from './7528/E7528';
-import * as E7530 from './7530/E7530';
-import * as E7592 from './7592/E7592';
-import * as E7676 from './7676/E7676';
+import * as U4E00 from './4E00/E4E00';
+import * as U4E3F from './4E3F/E4E3F';
+import * as U4E28 from './4E28/E4E28';
+import * as U4E36 from './4E36/E4E36';
+import * as U4E59 from './4E59/E4E59';
+import * as U4E85 from './4E85/E4E85';
+import * as U4E8C from './4E8C/E4E8C';
+import * as U4EA0 from './4EA0/E4EA0';
+import * as U4EBA from './4EBA/E4EBA';
+import * as U513F from './513F/E513F';
+import * as U5165 from './5165/E5165';
+import * as U516B from './516B/E516B';
+import * as U5182 from './5182/E5182';
+import * as U5196 from './5196/E5196';
+import * as U51AB from './51AB/E51AB';
+import * as U51E0 from './51E0/E51E0';
+import * as U51F5 from './51F5/E51F5';
+import * as U5200 from './5200/E5200';
+import * as U529B from './529B/E529B';
+import * as U52F9 from './52F9/E52F9';
+import * as U5315 from './5315/E5315';
+import * as U531A from './531A/E531A';
+import * as U5338 from './5338/E5338';
+import * as U5341 from './5341/E5341';
+import * as U535C from './535C/E535C';
+import * as U5369 from './5369/E5369';
+import * as U5382 from './5382/E5382';
+import * as U53B6 from './53B6/E53B6';
+import * as U53C8 from './53C8/E53C8';
+import * as U53E3 from './53E3/E53E3';
+import * as U56D7 from './56D7/E56D7';
+import * as U571F from './571F/E571F';
+import * as U58EB from './58EB/E58EB';
+import * as U5902 from './5902/E5902';
+import * as U590A from './590A/E590A';
+import * as U5915 from './5915/E5915';
+import * as U5927 from './5927/E5927';
+import * as U5973 from './5973/E5973';
+import * as U5B50 from './5B50/E5B50';
+import * as U5B80 from './5B80/E5B80';
+import * as U5BF8 from './5BF8/E5BF8';
+import * as U5C0F from './5C0F/E5C0F';
+import * as U5C22 from './5C22/E5C22';
+import * as U5C38 from './5C38/E5C38';
+import * as U5C6E from './5C6E/E5C6E';
+import * as U5C71 from './5C71/E5C71';
+import * as U5DDB from './5DDB/E5DDB';
+import * as U5DE5 from './5DE5/E5DE5';
+import * as U5DF1 from './5DF1/E5DF1';
+import * as U5DFE from './5DFE/E5DFE';
+import * as U5E7A from './5E7A/E5E7A';
+import * as U5E7F from './5E7F/E5E7F';
+import * as U5E72 from './5E72/E5E72';
+import * as U5EF4 from './5EF4/E5EF4';
+import * as U5EFE from './5EFE/E5EFE';
+import * as U5F0B from './5F0B/E5F0B';
+import * as U5F13 from './5F13/E5F13';
+import * as U5F50 from './5F50/E5F50';
+import * as U5F61 from './5F61/E5F61';
+import * as U5F73 from './5F73/E5F73';
+import * as U5FC3 from './5FC3/E5FC3';
+import * as U6208 from './6208/E6208';
+import * as U65A4 from './65A4/E65A4';
+import * as U65B9 from './65B9/E65B9';
+import * as U624B from './624B/E624B';
+import * as U652F from './652F/E652F';
+import * as U6237 from './6236/E6237';
+import * as U6534 from './6534/E6534';
+import * as U6587 from './6587/E6587';
+import * as U6597 from './6597/E6597';
+import * as U6B20 from './6B20/E6B20';
+import * as U6B62 from './6B62/E6B62';
+import * as U6B79 from './6B79/E6B79';
+import * as U6BB3 from './6BB3/E6BB3';
+import * as U6BCB from './6BCB/E6BCB';
+import * as U65E0 from './65E0/E65E0';
+import * as U65E5 from './65E5/E65E5';
+import * as U66F0 from './66F0/E66F0';
+import * as U6708 from './6708/E6708';
+import * as U6728 from './6728/E6728';
+import * as U6BD4 from './6BD4/E6BD4';
+import * as U6BDB from './6BDB/E6BDB';
+import * as U6C0F from './6C0F/E6C0F';
+import * as U6C14 from './6C14/E6C14';
+import * as U6C34 from './6C34/E6C34';
+import * as U706B from './706B/E706B';
+import * as U722A from './722A/E722A';
+import * as U723B from './723B/E723B';
+import * as U723F from './723F/E723F';
+import * as U7236 from './7236/E7236';
+import * as U72AC from './72AC/E72AC';
+import * as U74DC from './74DC/E74DC';
+import * as U74E6 from './74E6/E74E6';
+import * as U725B from './725B/E725B';
+import * as U751F from './751F/E751F';
+import * as U7247 from './7247/E7247';
+import * as U7259 from './7259/E7259';
+import * as U7384 from './7384/E7384';
+import * as U7389 from './7389/E7389';
+import * as U7518 from './7518/E7518';
+import * as U76AE from './76AE/E76AE';
+import * as U76BF from './76BF/E76BF';
+import * as U76EE from './76EE/E76EE';
+import * as U77DB from './77DB/E77DB';
+import * as U758B from './758B/E758B';
+import * as U767D from './767D/E767D';
+import * as U7528 from './7528/E7528';
+import * as U7530 from './7530/E7530';
+import * as U7592 from './7592/E7592';
+import * as U7676 from './7676/E7676';
 import * as U7A74 from './7A74/E7A74';
 import * as U7ACB from './7ACB/E7ACB';
 import * as U7AF9 from './7AF9/E7AF9';
@@ -120,116 +120,116 @@ import * as U79BE from './79BE/E79BE';
 import * as U793A from './793A/E793A';
 
 export const evolutionSvgs = {
-  U4E00: E4E00,
-  U4E3F: E4E3F,
-  U4E28: E4E28,
-  U4E36: E4E36,
-  U4E59: E4E59,
-  U4E85: E4E85,
-  U4E8C: E4E8C,
-  U4EA0: E4EA0,
-  U4EBA: E4EBA,
-  U513F: E513F,
-  U5165: E5165,
-  U516B: E516B,
-  U5182: E5182,
-  U5196: E5196,
-  U51AB: E51AB,
-  U51E0: E51E0,
-  U51F5: E51F5,
-  U5200: E5200,
-  U529B: E529B,
-  U52F9: E52F9,
-  U5315: E5315,
-  U531A: E531A,
-  U5338: E5338,
-  U5341: E5341,
-  U535C: E535C,
-  U5369: E5369,
-  U5382: E5382,
-  U53B6: E53B6,
-  U53C8: E53C8,
-  U53E3: E53E3,
-  U56D7: E56D7,
-  U571F: E571F,
-  U58EB: E58EB,
-  U5902: E5902,
-  U590A: E590A,
-  U5915: E5915,
-  U5927: E5927,
-  U5973: E5973,
-  U5B50: E5B50,
-  U5B80: E5B80,
-  U5BF8: E5BF8,
-  U5C0F: E5C0F,
-  U5C22: E5C22,
-  U5C38: E5C38,
-  U5C6E: E5C6E,
-  U5C71: E5C71,
-  U5DDB: E5DDB,
-  U5DE5: E5DE5,
-  U5DF1: E5DF1,
-  U5DFE: E5DFE,
-  U5E7A: E5E7A,
-  U5E7F: E5E7F,
-  U5E72: E5E72,
-  U5EF4: E5EF4,
-  U5EFE: E5EFE,
-  U5F0B: E5F0B,
-  U5F13: E5F13,
-  U5F50: E5F50,
-  U5F61: E5F61,
-  U5F73: E5F73,
-  U5FC3: E5FC3,
-  U6208: E6208,
-  U65A4: E65A4,
-  U65B9: E65B9,
-  U624B: E624B,
-  U652F: E652F,
-  U6237: E6237,
-  U6534: E6534,
-  U6587: E6587,
-  U6597: E6597,
-  U6B20: E6B20,
-  U6B62: E6B62,
-  U6B79: E6B79,
-  U6BB3: E6BB3,
-  U6BCB: E6BCB,
-  U65E0: E65E0,
-  U65E5: E65E5,
-  U66F0: E66F0,
-  U6708: E6708,
-  U6728: E6728,
-  U6BD4: E6BD4,
-  U6BDB: E6BDB,
-  U6C0F: E6C0F,
-  U6C14: E6C14,
-  U6C34: E6C34,
-  U706B: E706B,
-  U722A: E722A,
-  U723B: E723B,
-  U723F: E723F,
-  U7236: E7236,
-  U72AC: E72AC,
-  U74DC: E74DC,
-  U74E6: E74E6,
-  U725B: E725B,
-  U751F: E751F,
-  U7247: E7247,
-  U7259: E7259,
-  U7384: E7384,
-  U7389: E7389,
-  U7518: E7518,
-  U76AE: E76AE,
-  U76BF: E76BF,
-  U76EE: E76EE,
-  U77DB: E77DB,
-  U758B: E758B,
-  U767D: E767D,
-  U7528: E7528,
-  U7530: E7530,
-  U7592: E7592,
-  U7676: E7676,
+  U4E00,
+  U4E3F,
+  U4E28,
+  U4E36,
+  U4E59,
+  U4E85,
+  U4E8C,
+  U4EA0,
+  U4EBA,
+  U513F,
+  U5165,
+  U516B,
+  U5182,
+  U5196,
+  U51AB,
+  U51E0,
+  U51F5,
+  U5200,
+  U529B,
+  U52F9,
+  U5315,
+  U531A,
+  U5338,
+  U5341,
+  U535C,
+  U5369,
+  U5382,
+  U53B6,
+  U53C8,
+  U53E3,
+  U56D7,
+  U571F,
+  U58EB,
+  U5902,
+  U590A,
+  U5915,
+  U5927,
+  U5973,
+  U5B50,
+  U5B80,
+  U5BF8,
+  U5C0F,
+  U5C22,
+  U5C38,
+  U5C6E,
+  U5C71,
+  U5DDB,
+  U5DE5,
+  U5DF1,
+  U5DFE,
+  U5E7A,
+  U5E7F,
+  U5E72,
+  U5EF4,
+  U5EFE,
+  U5F0B,
+  U5F13,
+  U5F50,
+  U5F61,
+  U5F73,
+  U5FC3,
+  U6208,
+  U65A4,
+  U65B9,
+  U624B,
+  U652F,
+  U6237,
+  U6534,
+  U6587,
+  U6597,
+  U6B20,
+  U6B62,
+  U6B79,
+  U6BB3,
+  U6BCB,
+  U65E0,
+  U65E5,
+  U66F0,
+  U6708,
+  U6728,
+  U6BD4,
+  U6BDB,
+  U6C0F,
+  U6C14,
+  U6C34,
+  U706B,
+  U722A,
+  U723B,
+  U723F,
+  U7236,
+  U72AC,
+  U74DC,
+  U74E6,
+  U725B,
+  U751F,
+  U7247,
+  U7259,
+  U7384,
+  U7389,
+  U7518,
+  U76AE,
+  U76BF,
+  U76EE,
+  U77DB,
+  U758B,
+  U767D,
+  U7528,
+  U7530,
+  U7592,
+  U7676,
   U7A74,
   U7ACB,
   U7AF9,
